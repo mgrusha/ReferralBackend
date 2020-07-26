cb8db7d44422912e6c7cb853dfa2b0a48f92c1f8
Andrew <andrewkasparevich@gmail.com>
2020-07-26 19:13:51 +0200

    Add getters and setters to Service model

--- src/main/java/com/referralfinder/ReferralServer/models/Service.java
+++ src/main/java/com/referralfinder/ReferralServer/models/Service.java
@@ -20,4 +20,36 @@ public class Service {
 
     @OneToMany
     private List<Referral> books;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<Referral> getBooks() {
+        return books;
+    }
+
+    public void setBooks(List<Referral> books) {
+        this.books = books;
+    }
 }
